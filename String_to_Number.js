be484f494e5b02f45f44b7e1b77c3c97b54d210b
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-09-30 21:48:28 +0600

    Create String_to_Number.js

---
+++ String_to_Number.js
@@ -0,0 +1,35 @@
+// Converting String to Number
+    
+        let x ;
+
+        x = Number("102"); 
+        document.write(x + "<br>");
+        document.write(typeof (x) + "<br>");
+
+        x = Number("102.50"); 
+        document.write(x + "<br>");
+        document.write(typeof (x) + "<br>");
+
+        x = Number("     102"); 
+        document.write(x + "<br>");
+        document.write(typeof (x) + "<br>");
+
+        x = Number("102        "); 
+        document.write(x + "<br>");
+        document.write(typeof (x) + "<br>");
+
+        x = Number("    102     ");
+        document.write(x + "<br>");
+        document.write(typeof (x) + "<br>");
+
+        x = Number("102,10");
+        document.write(x + "<br>");
+        document.write(typeof (x) + "<br>");
+
+        x = Number("102    100");
+        document.write(x + "<br>");
+        document.write(typeof (x) + "<br>");
+
+        x = Number("Rakib");
+        document.write(x + "<br>");
+        document.write(typeof (x) + "<br>");

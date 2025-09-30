0f26e6de36ddf5b70107af55ccbf7fe0856a5a58
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-09-30 21:30:37 +0600

    Update 1_intro.js

--- JavaScript Practice/1_intro.js
+++ JavaScript Practice/1_intro.js
@@ -1,1 +1,11 @@
 
+//Taking an Input from User
+
+        const name=prompt("Enter your name");
+        document.write("Hello, "+name);
+
+        const age=prompt("Enter your age");
+        document.write(", "+age);
+
+        const address=prompt("Enter your address");
+        document.write(", "+address);

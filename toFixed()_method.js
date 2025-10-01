dcfa35439e96d7e757cfc5f313e3e6f97bea6bbc
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-10-01 21:29:38 +0600

    Create toFixed()_method.js

---
+++ toFixed()_method.js
@@ -0,0 +1,11 @@
+       let x = 23.21341245346;
+       let s;
+
+       s = x.toFixed();
+       document.write( s + " Type of: " + typeof (s) + "<br>" );
+
+       s = x.toFixed(4);
+       document.write( s + " Type of: " + typeof (s) + "<br>" );
+
+       s = x.toFixed(8);
+       document.write( s + " Type of: " + typeof (s) + "<br>" );

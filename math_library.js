3dbc9e17c4a8cdf77c02a05f1f1bd6631074dc51
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-10-01 21:38:55 +0600

    Create math_library.js

---
+++ math_library.js
@@ -0,0 +1,14 @@
+
+        let x;
+
+        x = Math.PI; // 3.141592653589793
+        document.write(x + "<br>");
+
+        x = Math.E; // 2.718281828459045
+        document.write(x + "<br>");
+
+        x = Math.random(); // Random number between 0 and 1
+        document.write(x + "<br>");
+
+        x = parseInt(Math.random() * 100); //Random integer between 0 and 99
+        document.write(x + "<br>");

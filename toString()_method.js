6a8693f32e5b0afc28d3abe49d9cc3051cc83c45
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-10-01 21:28:32 +0600

    Create toString()_method.js

---
+++ toString()_method.js
@@ -0,0 +1,18 @@
+
+       let x = 23;
+       let s;
+
+       s = x.toString();
+       document.write( s + " Type of: " + typeof (s) + "<br>" );
+
+       s = x.toString(2);
+       document.write( s + " Type of: " + typeof (s) + "<br>" );
+
+       s = x.toString(8);
+       document.write( s + " Type of: " + typeof (s) + "<br>" );
+
+       s = x.toString(10);
+       document.write( s + " Type of: " + typeof (s) + "<br>" );
+
+       s = x.toString(16);
+       document.write( s + " Type of: " + typeof (s) + "<br>" );

7b738b766c50168506c623856fe913febe0096ce
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-10-01 21:30:33 +0600

    Create toExponential()_method.js

---
+++ toExponential()_method.js
@@ -0,0 +1,12 @@
+
+        let x = 33.2134353523;
+        let s;
+
+        s = x.toExponential();
+        document.write(s + " type of: " + typeof (s) + "<br>");
+
+        s = x.toExponential(4);
+        document.write(s + " type of: " + typeof (s) + "<br>");
+
+        s = x.toExponential(8);
+        document.write(s + " type of: " + typeof (s) + "<br>");

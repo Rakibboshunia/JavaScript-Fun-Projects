aaac814f5509a66272ca92f15b9b92602ec3051a
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-10-04 16:11:24 +0600

    Create Rounding_methods.js

---
+++ Rounding_methods.js
@@ -0,0 +1,16 @@
+// Rounding Methods  
+
+        let x = 7.6;
+        let s;
+
+        s = Math.ceil(x); // Rounds up
+        document.write(s + "<br>");
+
+        s = Math.floor(x); // Rounds down
+        document.write(s + "<br>");
+
+        s = Math.round(x); // Rounds to nearest integer
+        document.write(s + "<br>");
+
+        s = Math.trunc(x); // Removes decimal part
+        document.write(s + "<br>");

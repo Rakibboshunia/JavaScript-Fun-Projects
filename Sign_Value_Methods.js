2e0a7d334906a97a8732dce522791ee268b211fe
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-10-04 16:11:53 +0600

    Create Sign_Value_Methods.js

---
+++ Sign_Value_Methods.js
@@ -0,0 +1,18 @@
+// Sign Value Methods
+
+        x = 5;
+        y = -5;
+
+        s = Math.sign(x); // Returns 1 if positive, -1 if negative, 0 if zero
+        document.write(s + "<br>");
+
+        s = Math.sign(y); // Returns 1 if positive, -1 if negative, 0 if zero
+        document.write(s + "<br>");
+
+        // Absolute Value Methods
+
+        s = Math.abs(x); // Returns 1 if positive, -1 if negative, 0 if zero
+        document.write(s + "<br>");
+
+        s = Math.abs(y); // Returns 1 if positive, -1 if negative, 0 if zero
+        document.write(s + "<br>");

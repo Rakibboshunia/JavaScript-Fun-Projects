f00c06d1c1d4977022d2835477993a3e6370ac89
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-10-04 16:12:26 +0600

    Create Power_Value_Methods.js

---
+++ Power_Value_Methods.js
@@ -0,0 +1,16 @@
+// Power Value Methods
+
+        s = Math.pow(2, 3);
+        document.write(s + "<br>");
+
+        s = Math.sqrt(16);
+        document.write(s + "<br>");
+
+
+        // Min and Max Value Methods
+
+        s = Math.max(8,3,5,29,40,-45);
+        document.write(s + "<br>");
+
+        s = Math.min(3,8,23,9,-45,-98,32);
+        document.write( s + "<br>");

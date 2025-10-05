828b8d5f770c83457474513d0308d489f625ea00
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-10-05 17:38:08 +0600

    Create switch case.js

---
+++ switch case.js
@@ -0,0 +1,37 @@
+// Day of the week using switch case statement 
+
+        let date = new Date();
+        let day = date.getDay();
+
+        switch(day){
+            case 0:
+                document.write("Today is Sunday");
+                break;
+
+            case 1:
+                document.write("Today is Monday");
+                break;
+
+            case 2:
+                document.write("Today is Tuesday");
+                break;
+
+            case 3:
+                document.write("Today is Wednesday");
+                break;
+
+            case 4:
+                document.write("Today is Thursday");
+                break;
+
+            case 5:
+                document.write("Today is Friday");
+                break;
+
+            case 6:
+                document.write("Today is Saturday");
+                break;
+
+            default:
+                document.write("Unknown day");
+        }

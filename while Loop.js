3a23ca36011242c589d9a9e89613afff45fa7e01
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-10-05 17:39:36 +0600

    Create while Loop.js

---
+++ while Loop.js
@@ -0,0 +1,8 @@
+//while loop
+      
+        let i = 0;
+
+        while(i < 10){
+            document.write("I am learning JavaScript<br>" + "Value of i is: 		" + i + "<br>");
+            i = i+1;
+        }

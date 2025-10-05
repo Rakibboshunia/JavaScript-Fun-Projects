8b8416597cf22829dadbe355515085fb9dd3759e
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-10-05 17:38:41 +0600

    Create switch Excercise.js

---
+++ switch Excercise.js
@@ -0,0 +1,14 @@
+//weekend or weekday using switch case statement
+        
+        let date = new Date();
+        let day = date.getDay();
+
+        switch(day){
+            case 5:
+            case 6:
+                document.write("Weekend" + "<br>");
+                break;
+
+            default:
+                document.write("weekday" + "<br>");
+        }

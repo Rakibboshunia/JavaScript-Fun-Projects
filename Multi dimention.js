e01e6f62e3db7c169ff656eb07a4c6dc71adb0d4
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-10-07 00:31:44 +0600

    Create Multi dimention.js

---
+++ Multi dimention.js
@@ -0,0 +1,8 @@
+// Multi dimention array
+
+let arr = [
+    ['rakib', 'sajib', 'asif'],
+    ['ashik', 'fahim', 'sabbir'],
+    ['munna', 'salam', ['lalam', 'malam', 'palam']],
+];
+console.log(arr[1][2]);

5a48be9fd331c85c6c19492c32e7ee9f1ef2aca1
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-10-06 21:46:21 +0600

    Create break in loops.js

---
+++ break in loops.js
@@ -0,0 +1,9 @@
+for(let i=0; i<10; i++){ 
+            document.write('statment 1: ' + i + "<br>"); //
+            if(i == 6){  // even number
+                break;  // exit the loop
+            }
+
+            document.write('statment 2: ' + i + "<br>"); // odd number
+
+        }

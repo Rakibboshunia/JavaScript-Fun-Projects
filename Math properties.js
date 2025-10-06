2a836bd078f25e9857c2fb70ecf7d1fa59a3ceb8
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-10-07 00:33:37 +0600

    Create Math properties.js

---
+++ Math properties.js
@@ -0,0 +1,28 @@
+// Math.E        // returns Euler's number
+// Math.PI       // returns PI
+// Math.SQRT2    // returns the square root of 2
+// Math.SQRT1_2  // returns the square root of 1/2
+// Math.LN2      // returns the natural logarithm of 2
+// Math.LN10     // returns the natural logarithm of 10
+// Math.LOG2E    // returns base 2 logarithm of E
+// Math.LOG10E   // returns base 10 logarithm of E
+
+let num = 34.23423;
+
+console.log(Math.E); 
+
+console.log(Math.PI); 
+
+console.log(Math.SQRT2); 
+
+console.log(Math.SQRT1_2); 
+
+console.log(Math.LN2); 
+
+console.log(Math.LN10); 
+
+console.log(Math.LOG2E); 
+
+console.log(Math.LOG10E); 
+
+console.log(Math.sign(num));

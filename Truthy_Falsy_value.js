2d828015a9d9a2cc1b47db132b0233c0f006e6b0
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-10-08 22:34:32 +0600

    Create Truthy_Falsy_value.js

---
+++ Truthy_Falsy_value.js
@@ -0,0 +1,24 @@
+
+// Falsy values in JavaScript
+/*
+null,
+undefined,
+0,
+-0,
+NaN,
+"",
+false,
+0n
+*/
+
+if(true){
+    console.log("This is truthy");
+}
+
+if(['hello']){
+    console.log("This is truthy");
+}
+
+if(false){
+    console.log("This is falsy");
+}

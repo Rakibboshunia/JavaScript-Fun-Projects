cbc10a244f7c261995f9fed2a56789445e34ae0a
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-10-08 22:32:54 +0600

    Create arithmatic_operator.js

---
+++ arithmatic_operator.js
@@ -0,0 +1,45 @@
+
+// Arithmatic Operators in JavaScript
+
+// Addition (+)
+
+let a = 10;
+let b = 5;
+
+console.log("Addition: " + (a + b)); 
+
+// Subtraction (-)
+
+console.log("Subtraction: " + (a - b));
+
+// Multiplication (*)
+
+console.log("Multiplication: " + (a * b));
+
+// Division (/)
+
+console.log("Division: " + (a / b));
+
+// Modulus (%)
+
+console.log("Modulus: " + (a % b));  // Remainder of 10 divided by 5 is 0
+
+// Exponentiation (**)
+
+console.log("Exponentiation: " + (a ** b)); // 10^5 = 100000
+
+// Increment (++)
+
+let c = 10;
+console.log("Pre-increment: " + (++c));  // Pre-increment
+
+let d = 15;
+console.log("Post-increment: " + (d++));  // Post-increment
+
+// Decrement (--)
+
+let e = 20;
+console.log("Pre-decrement: " + (--e));  // Pre-decrement
+
+let f = 25;
+console.log("Post-decrement: " + (f--));  // Post-decrement

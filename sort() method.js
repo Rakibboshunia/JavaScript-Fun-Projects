e7b0783be9bdcef065885a91bd30d3a701d7df9f
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-10-31 23:45:13 +0600

    Update sort() method.js

--- sort() method.js
+++ sort() method.js
@@ -1,6 +1,6 @@
 // sort() method sorts the elements of an array in place and returns the 	sorted array. 
         
-        let number = [20, 12, 24,true, false, 66,23, 90, 10, 45,"Raihan", 		"Arafat", "Sakib", "Jahid"];
+        let number = [20, 12, 24,true, false, 66,23, 90, 10, 45,"Raihan", "Arafat", "Sakib", "Jahid"];
         
         document.write("Original array: " + number + "<br>");
 
@@ -14,4 +14,4 @@
 
         //Another way to sort from large to small
         number.sort().reverse();
-        document.write("Sorted array in descending order: " + number + 			"<br>");
+        document.write("Sorted array in descending order: " + number + 	"<br>");

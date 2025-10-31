74aedad046b3a619315fb10aeb94e3878f40cac3
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-10-31 23:43:28 +0600

    Update concat() method.js

--- concat() method.js
+++ concat() method.js
@@ -1,7 +1,7 @@
-// concat() method is used to merge two or more arrays. This method does not 	change the existing arrays, but instead returns a new array.
+// concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
         
-        var array1 = [10,20,30,40];
-        var array2 = [50,60,70,80];
+var array1 = [10,20,30,40];
+var array2 = [50,60,70,80];
 
-        var array3 = 	array1.concat(array2,array1,"Rakib","Ashik",200,500,'A','X'); // 		Merges two arrays and adds two strings and two numbers and 		two characters
-        document.write(array3);
+var array3 = 	array1.concat(array2,array1,"Rakib","Ashik",200,500,'A','X'); // Merges two arrays and adds two strings and two numbers and two characters
+document.write(array3);

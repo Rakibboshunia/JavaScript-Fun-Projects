02fac5a67164e8288e81ef2ddb0c02a8bd78ff04
MD. AL RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2025-11-03 20:18:50 +0600

    Update Nested loops brack and continue.js

--- Nested loops brack and continue.js
+++ Nested loops brack and continue.js
@@ -3,22 +3,19 @@
 
         for(let i=0; i<5; i++){
             for( let j=0; j<5; j++){
-
-            document.write('statment 1: i: ' + i + ", j: " + j + "<br>");
+                    document.write('statment 1: i: ' + i + ", j: " + j + "<br>");
                  
             if(i == 6){  // even number
                 break;  // exit the loop
             }
-
-            document.write('statment 2: i: ' + i + ", j: " + j + "<br>"); 
+                document.write('statment 2: i: ' + i + ", j: " + j + "<br>"); 
             }
         } 
 
 
         for(let i=0; i<5; i++){
             for( let j=0; j<5; j++){
-
-                document.write('statment 1: i: ' + i + ", j: " + j + 			"<br>");
+                document.write('statment 1: i: ' + i + ", j: " + j + "<br>");
                  
             if(i == 6){  // even number
                 continue;  // exit the loop
